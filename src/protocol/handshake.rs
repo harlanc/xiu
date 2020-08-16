e9fca5cd379d9fe5ede84ba87f43c23a4ad11c6f
harlanc <wawacry@qq.com>
2020-08-16 22:35:36 +0800

    add handshake impl

---
+++ src/protocol/handshake.rs
@@ -0,0 +1,27 @@
+
+
+struct  SimpleHandshake{
+
+}
+
+struct ComplexHandshake{
+
+}
+
+
+impl SimpleHandshake {
+    fn new() -> SimpleHandshake{
+        SimpleHandshake{
+
+        }
+    }
+
+    fn handshake_with_client(){
+
+    }
+
+    fn handshake_with_server(){
+        
+    }
+
+}
