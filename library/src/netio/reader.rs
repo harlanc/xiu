ca3d45f62bbd28e11bc5edf3d6b204c5f79cd5dc
HarlanC <wawacry@qq.com>
2021-01-17 18:36:37 +0800

    add some funcs

--- library/src/netio/reader.rs
+++ library/src/netio/reader.rs
@@ -1,5 +1,5 @@
 use byteorder::{ByteOrder, ReadBytesExt};
-use bytes::BytesMut;
+use bytes::{BufMut, BytesMut};
 use std::io;
 use std::io::Cursor;
 
@@ -51,31 +51,40 @@ impl Reader {
         Ok(self.buffer.split_to(bytes_num))
     }
 
+    pub fn advance_bytes(&mut self, bytes_num: usize) -> Result<BytesMut, IOReadError> {
+        if self.buffer.len() < bytes_num {
+            return Err(IOReadError {
+                value: IOReadErrorValue::NotEnoughBytes,
+            });
+        }
+
+        //here maybe optimised
+        Ok(self.buffer.clone().split_to(bytes_num))
+    }
+
     pub fn read_bytes_cursor(&mut self, bytes_num: usize) -> Result<Cursor<BytesMut>, IOReadError> {
         let tmp_bytes = self.read_bytes(bytes_num)?;
         let tmp_cursor = Cursor::new(tmp_bytes);
         Ok(tmp_cursor)
     }
 
-    pub fn only_read_u8(&mut self) -> Result<u8, IOReadError> {
-        let val = self.buffer.get(0);
-
-        match val {
-            None => {
-                return Err(IOReadError {
-                    value: IOReadErrorValue::NotEnoughBytes,
-                })
-            }
-            Some(v) => return Ok(v.clone()),
-        }
+    pub fn advance_bytes_cursor(&mut self, bytes_num: usize) -> Result<Cursor<BytesMut>, IOReadError> {
+        let tmp_bytes = self.advance_bytes(bytes_num)?;
+        let tmp_cursor = Cursor::new(tmp_bytes);
+        Ok(tmp_cursor)
     }
-
+    
     pub fn read_u8(&mut self) -> Result<u8, IOReadError> {
         let mut cursor = self.read_bytes_cursor(1)?;
 
         Ok(cursor.read_u8()?)
     }
 
+    pub fn advance_u8(&mut self) -> Result<u8, IOReadError> {
+        let mut cursor = self.advance_bytes_cursor(1)?;
+        Ok(cursor.read_u8()?)
+    }
+    
     pub fn read_u16<T: ByteOrder>(&mut self) -> Result<u16, IOReadError> {
         let mut cursor = self.read_bytes_cursor(2)?;
         let val = cursor.read_u16::<T>()?;
@@ -88,6 +97,11 @@ impl Reader {
         Ok(val)
     }
 
+    pub fn advance_u24<T: ByteOrder>(&mut self) -> Result<u32, IOReadError> {
+        let mut cursor = self.advance_bytes_cursor(3)?;
+        Ok(cursor.read_u24::<T>()?)
+    }
+    
     pub fn read_u32<T: ByteOrder>(&mut self) -> Result<u32, IOReadError> {
         let mut cursor = self.read_bytes_cursor(4)?;
         let val = cursor.read_u32::<T>()?;
