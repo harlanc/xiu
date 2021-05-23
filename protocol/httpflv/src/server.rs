0c504437428e349e58d0efa2ef53c3a75aef57c7
HarlanC <wawacry@qq.com>
2021-05-23 18:23:19 +0800

    coding for httpflv protocol

--- protocol/httpflv/src/server.rs
+++ protocol/httpflv/src/server.rs
@@ -2,8 +2,12 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{header, Body, Client, Method, Request, Response, Server, StatusCode};
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+use hyper::client::HttpConnector;
+use futures_util::{stream, StreamExt};
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
+
+type Result<T> = std::result::Result<T, GenericError>;
 
 static INDEX: &[u8] = b"<a href=\"test.html\">test.html</a>";
 static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
@@ -11,58 +15,66 @@ static NOTFOUND: &[u8] = b"Not Found";
 static POST_DATA: &str = r#"{"original": "data"}"#;
 static URL: &str = "http://127.0.0.1:1337/json_api";
 
-pub struct HttpFlvServer {
-    port: u32,
+async fn client_request_response(client: &Client<HttpConnector>) -> Result<Response<Body>> {
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(URL)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(POST_DATA.into())
+        .unwrap();
+
+    let web_res = client.request(req).await?;
+    // Compare the JSON we sent (before) with what we received (after):
+    let before = stream::once(async {
+        Ok(format!(
+            "<b>POST request body</b>: {}<br><b>Response</b>: ",
+            POST_DATA,
+        )
+        .into())
+    });
+    let after = web_res.into_body();
+    let body = Body::wrap_stream(before.chain(after));
+
+    Ok(Response::new(body))
 }
 
-impl HttpFlvServer {
-    pub fn new(port: u32) -> Self {
-        Self { port: port }
-    }
+async fn response_examples(
+    req: Request<Body>,
+    client: Client<HttpConnector>,
+) -> Result<Response<Body>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/test.html") => client_request_response(&client).await,
 
-    async fn api_get_response(&mut self) -> Result<Response<Body>, Error> {
-        let data = vec!["foo", "bar"];
-        let res = match serde_json::to_string(&data) {
-            Ok(json) => Response::builder()
-                .header(header::CONTENT_TYPE, "application/json")
-                .body(Body::from(json))
-                .unwrap(),
-            Err(_) => Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(INTERNAL_SERVER_ERROR.into())
-                .unwrap(),
-        };
-        Ok(res)
-    }
-
-    async fn route(&'static mut self, req: Request<Body>) -> Result<Response<Body>, Error> {
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/json_api") => self.api_get_response().await,
-            _ => {
-                // Return 404 not found response.
-                Ok(Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(NOTFOUND.into())
-                    .unwrap())
-            }
+        _ => {
+            // Return 404 not found response.
+            Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(NOTFOUND.into())
+                .unwrap())
         }
     }
+}
 
-    pub async fn run(&'static mut self) {
-        let new_service = make_service_fn(move |_| {
-            // Move a clone of `client` into the `service_fn`.
+async fn run() {
+    let addr = "127.0.0.1:1337".parse().unwrap();
 
-            async {
-                Ok::<_, GenericError>(service_fn(move | req| {
-                    // Clone again to ensure that client outlives this closure.
-                    self.route(req)
-                }))
-            }
-        });
+    // Share a `Client` with all `Service`s
+    let client = Client::new();
 
-        let addr = "127.0.0.1:1337".parse().unwrap();
-        let server = Server::bind(&addr).serve(new_service);
-        println!("Listening on http://{}", addr);
-        let _ = server.await;
-    }
+    let new_service = make_service_fn(move |_| {
+        // Move a clone of `client` into the `service_fn`.
+        let client = client.clone();
+        async {
+            Ok::<_, GenericError>(service_fn(move |req| {
+                // Clone again to ensure that client outlives this closure.
+                response_examples(req, client.to_owned())
+            }))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(new_service);
+
+    println!("Listening on http://{}", addr);
+
+    server.await;
 }
