3e97cd37b47f7e17ba7f08d2be4898ebfaada147
HarlanC <wawacry@qq.com>
2021-08-07 22:15:42 +0800

    flush to file

--- protocol/hls/src/m3u8.rs
+++ protocol/hls/src/m3u8.rs
@@ -2,6 +2,8 @@ use std::{collections::VecDeque, fmt::format};
 
 use rtmp::messages::define::msg_type_id;
 
+use std::{fs::File, io::Write};
+
 pub struct Segment {
     pts: i64,
     /*ts duration*/
@@ -36,10 +38,15 @@ pub struct M3u8 {
     live_ts_count: usize,
 
     segments: VecDeque<Segment>,
+    m3u8_header: String,
+
+    m3u8_file_handler: File,
 }
 
 impl M3u8 {
-    pub fn new(duration: i64, live_ts_count: usize) -> Self {
+    pub fn new(duration: i64, live_ts_count: usize, name: String) -> Self {
+        let file_handler = File::create(name).unwrap();
+
         Self {
             version: 3,
             sequence_no: 0,
@@ -47,8 +54,11 @@ impl M3u8 {
             is_live: true,
             live_ts_count,
             segments: VecDeque::new(),
+            m3u8_header: String::new(),
+            m3u8_file_handler: file_handler,
         }
     }
+    pub fn flush(&mut self) {}
     pub fn add_segment(&mut self, name: String, pts: i64, duration: i64, discontinuity: bool) {
         let segment_count = self.segments.len();
 
@@ -59,11 +69,19 @@ impl M3u8 {
         self.duration = std::cmp::max(duration, self.duration);
         self.sequence_no += 1;
 
-        let segment = Segment::new(pts, duration, discontinuity, name);
+        let segment = Segment::new(pts, duration, discontinuity, name.clone());
         self.segments.push_back(segment);
+
+        /*flush to file*/
+        let mut segment_content: String = String::from("");
+        if discontinuity {
+            segment_content += "#EXT-X-DISCONTINUITY\n";
+        }
+        segment_content += format!("#EXTINF:{:.3}\n{}\n", duration as f64 / 1000.0, name).as_str();
+        self.m3u8_file_handler.write(segment_content.as_bytes());
     }
 
-    pub fn generate_m3u8_content(&mut self) -> String {
+    fn init_m3u8_header(&mut self) {
         let mut playlist_type: &str = "";
         let mut allow_cache: &str = "";
         if !self.is_live {
@@ -71,14 +89,20 @@ impl M3u8 {
             allow_cache = "#EXT-X-ALLOW-CACHE:YES\n";
         }
 
-        let mut m3u8_content: String = String::new();
-        m3u8_content = format!("#EXTM3U\n");
-        m3u8_content += format!("#EXT-X-VERSION:{}\n", self.version).as_str();
-        m3u8_content +=
+        self.m3u8_header = format!("#EXTM3U\n");
+        self.m3u8_header += format!("#EXT-X-VERSION:{}\n", self.version).as_str();
+        self.m3u8_header +=
             format!("#EXT-X-TARGETDURATION:{}\n", (self.duration + 999) / 1000).as_str();
-        m3u8_content += format!("#EXT-X-MEDIA-SEQUENCE:{}\n", self.sequence_no).as_str();
-        m3u8_content += playlist_type;
-        m3u8_content += allow_cache;
+        self.m3u8_header += format!("#EXT-X-MEDIA-SEQUENCE:{}\n", self.sequence_no).as_str();
+        self.m3u8_header += playlist_type;
+        self.m3u8_header += allow_cache;
+
+        /*flush to file*/
+        self.m3u8_file_handler.write(self.m3u8_header.as_bytes());
+    }
+
+    pub fn generate_m3u8_content(&mut self) -> String {
+        let mut m3u8_content = self.m3u8_header.clone();
 
         for segment in &self.segments {
             if segment.discontinuity {
