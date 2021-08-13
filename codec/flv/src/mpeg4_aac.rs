d68ec3c234b01286e4a4717218d7131baefabfb9
HarlanC <wawacry@qq.com>
2021-08-13 18:13:11 +0800

    fix and audio parse impl

--- codec/flv/src/mpeg4_aac.rs
+++ codec/flv/src/mpeg4_aac.rs
@@ -1,3 +1,4 @@
+use super::bitvec as mpeg4bitvec;
 use super::bitvec::Mpeg4BitVec;
 use super::define::h264_nal_type;
 use super::errors::MpegAacError;
@@ -84,24 +85,24 @@ impl Mpeg4AacProcessor {
     }
 
     pub fn audio_specific_config_load(&mut self) -> Result<(), MpegAacError> {
-        let byte_0 = self.bytes_reader.read_u8()?;
+        let byte_0 = self.bytes_reader.advance_u8()?;
         self.mpeg4_aac.profile = (byte_0 >> 3) & 0x1F;
 
-        let byte_1 = self.bytes_reader.read_u8()?;
+        let byte_1 = self.bytes_reader.advance_u8()?;
         self.mpeg4_aac.sampling_frequency_index = ((byte_0 & 0x07) << 1) | ((byte_1 >> 7) & 0x01);
         self.mpeg4_aac.channel_configuration = (byte_1 >> 3) & 0x0F;
         self.mpeg4_aac.channels = self.mpeg4_aac.channel_configuration;
         self.mpeg4_aac.sampling_frequency =
             AAC_FREQUENCE[self.mpeg4_aac.sampling_frequency_index as usize];
 
-        if 0 == self.mpeg4_aac.channel_configuration {
-            print!("bytes**************************=========\n");
-        }
+        // if self.bytes_reader.len() > 2 {
+        //     return self.audio_specific_config_load2();
+        // }
 
-        let length = self.bytes_reader.len();
-        if self.bytes_reader.len() > 0 {
-            print!("bytes elft**************************=========\n");
-        }
+        self.bytes_reader.read_u8()?;
+        self.bytes_reader.read_u8()?;
+
+        self.bytes_reader.get_remaining_bytes();
 
         Ok(())
     }
@@ -136,29 +137,244 @@ impl Mpeg4AacProcessor {
             extension_audio_object_type = 0;
         }
 
+        let ep_config: u64;
+
         match self.mpeg4_aac.profile {
-            1 | 2 | 3 | 4 | 5 | 6 | 7 | 17 | 19 | 20 | 21 | 22 | 23 => {}
+            1 | 2 | 3 | 4 | 5 | 6 | 7 | 17 | 19 | 20 | 21 | 22 | 23 => {
+                self.ga_specific_config_load()?;
+            }
+            8 => {
+                self.celp_specific_config_load()?;
+            }
             _ => {}
         }
+
+        match self.mpeg4_aac.profile {
+            17 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 39 => {
+                ep_config = self.bits_data.read_n_bits(2)?;
+
+                match ep_config {
+                    2 | 3 => {
+                        return Err(MpegAacError {
+                            value: MpegAacErrorValue::ShouldNotComeHere,
+                        });
+                    }
+
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+
+        let mut sync_extension_type: u64;
+
+        if 5 != extension_audio_object_type && self.bits_data.len() >= 16 {
+            sync_extension_type = self.bits_data.read_n_bits(11)?;
+
+            if 0x2B7 == sync_extension_type {
+                extension_audio_object_type = self.get_audio_object_type()?;
+
+                match extension_audio_object_type {
+                    5 => {
+                        self.mpeg4_aac.sbr = self.bits_data.read_n_bits(1)? as usize;
+                        if self.mpeg4_aac.sbr > 0 {
+                            extension_sampling_frequency_index = self.get_sampling_frequency()?;
+                            if self.bits_data.len() >= 12 {
+                                sync_extension_type = self.bits_data.read_n_bits(11)?;
+                                if 0x548 == sync_extension_type {
+                                    self.mpeg4_aac.ps = self.bits_data.read_n_bits(1)? as usize;
+                                }
+                            }
+                        }
+                    }
+                    22 => {
+                        self.mpeg4_aac.sbr = self.bits_data.read_n_bits(1)? as usize;
+
+                        if self.mpeg4_aac.sbr > 0 {
+                            extension_sampling_frequency_index = self.get_sampling_frequency()?;
+                        }
+
+                        extension_channel_configuration = self.bits_data.read_n_bits(4)? as u8;
+                    }
+
+                    _ => {}
+                }
+            }
+        }
+
+        self.bits_data
+            .bits_aligment(8, mpeg4bitvec::BitVectorOpType::Read)?;
 
         Ok(())
     }
 
-    pub fn ga_specific_config_load(&mut self) -> Result<u8, MpegAacError> {
+    pub fn celp_specific_config_load(&mut self) -> Result<(), MpegAacError> {
+        let excitation_mode: u64;
+
+        if self.bits_data.read_n_bits(1)? > 0 {
+            excitation_mode = self.bits_data.read_n_bits(1)?;
+            self.bits_data.read_n_bits(1)?;
+            self.bits_data.read_n_bits(1)?;
+
+            if excitation_mode == 1 {
+                self.bits_data.read_n_bits(3)?;
+            } else if excitation_mode == 0 {
+                self.bits_data.read_n_bits(5)?;
+                self.bits_data.read_n_bits(2)?;
+                self.bits_data.read_n_bits(1)?;
+            }
+        } else {
+            if self.bits_data.read_n_bits(1)? > 0 {
+                self.bits_data.read_n_bits(2)?;
+            } else {
+                self.bits_data.read_n_bits(2)?;
+            }
+        }
+
+        Ok(())
+    }
+    pub fn ga_specific_config_load(&mut self) -> Result<(), MpegAacError> {
+        let extension_flag: u64;
         self.bits_data.read_n_bits(1)?;
 
         if self.bits_data.read_n_bits(1)? > 0 {
             self.bits_data.read_n_bits(14)?;
         }
+        extension_flag = self.bits_data.read_n_bits(1)?;
 
         if 0 == self.mpeg4_aac.channel_configuration {
-            let mut cur_bits_data = Mpeg4BitVec::new();
-            cur_bits_data.extend_from_bytesmut(self.mpeg4_aac.pce.clone());
-
-            //self.mpeg4_aac.npce = cur_bits_data
+            self.pce_load()?;
         }
 
-        Ok(0)
+        if self.mpeg4_aac.profile == 6 || self.mpeg4_aac.profile == 20 {
+            self.bits_data.read_n_bits(3)?;
+        }
+
+        if extension_flag > 0 {
+            match self.mpeg4_aac.profile {
+                22 => {
+                    self.bits_data.read_n_bits(5)?;
+                    self.bits_data.read_n_bits(11)?;
+                }
+                17 | 19 | 20 | 23 => {
+                    self.bits_data.read_n_bits(1)?;
+                    self.bits_data.read_n_bits(1)?;
+                    self.bits_data.read_n_bits(1)?;
+                }
+                _ => {}
+            }
+
+            self.bits_data.read_n_bits(1)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn pce_load(&mut self) -> Result<u8, MpegAacError> {
+        let i: u64;
+        let mut cpe: u64;
+        let mut tag: u64;
+        let element_instance_tag: u64;
+        let object_type: u64;
+        let sampling_frequency_index: u64;
+        let num_front_channel_elements: u64;
+        let num_side_channel_elements: u64;
+        let num_back_channel_elements: u64;
+        let num_lfe_channel_elements: u64;
+        let num_assoc_data_elements: u64;
+        let num_valid_cc_elements: u64;
+        let comment_field_bytes: u64;
+
+        let mut pce_bits_vec = Mpeg4BitVec::new();
+        pce_bits_vec.extend_from_bytesmut(self.mpeg4_aac.pce.clone());
+
+        self.mpeg4_aac.channels = 0;
+
+        element_instance_tag =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        object_type = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 2)?;
+        sampling_frequency_index =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        num_front_channel_elements =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        num_side_channel_elements =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        num_back_channel_elements =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        num_lfe_channel_elements =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 2)?;
+        num_assoc_data_elements =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 3)?;
+        num_valid_cc_elements =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+
+        for _ in 0..3 {
+            if mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 1)? > 0 {
+                mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+            }
+        }
+
+        for _ in 0..num_front_channel_elements {
+            cpe = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 1)?;
+            tag = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+
+            if cpe > 0 || self.mpeg4_aac.ps > 0 {
+                self.mpeg4_aac.channels += 2;
+            } else {
+                self.mpeg4_aac.channels += 1;
+            }
+        }
+
+        for _ in 0..num_side_channel_elements {
+            cpe = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 1)?;
+            tag = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+
+            if cpe > 0 || self.mpeg4_aac.ps > 0 {
+                self.mpeg4_aac.channels += 2;
+            } else {
+                self.mpeg4_aac.channels += 1;
+            }
+        }
+
+        for _ in 0..num_back_channel_elements {
+            cpe = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 1)?;
+            tag = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+
+            if cpe > 0 || self.mpeg4_aac.ps > 0 {
+                self.mpeg4_aac.channels += 2;
+            } else {
+                self.mpeg4_aac.channels += 1;
+            }
+        }
+
+        for _ in 0..num_lfe_channel_elements {
+            tag = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+            self.mpeg4_aac.channels += 1;
+        }
+
+        for _ in 0..num_assoc_data_elements {
+            tag = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        }
+
+        for _ in 0..num_valid_cc_elements {
+            cpe = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 1)?;
+            tag = mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 4)?;
+        }
+
+        pce_bits_vec.bits_aligment(8, mpeg4bitvec::BitVectorOpType::Write)?;
+        self.bits_data
+            .bits_aligment(8, mpeg4bitvec::BitVectorOpType::Read)?;
+
+        comment_field_bytes =
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 8)?;
+
+        for _ in 0..comment_field_bytes {
+            mpeg4bitvec::mpeg4_bits_copy(&mut pce_bits_vec, &mut self.bits_data, 8)?;
+        }
+
+        let rv = (pce_bits_vec.write_offset + 7) / 8;
+
+        Ok(rv as u8)
     }
 
     pub fn get_audio_object_type(&mut self) -> Result<u8, MpegAacError> {
