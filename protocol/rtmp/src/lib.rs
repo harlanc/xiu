ea270d9632f7f70b23c9ee8a86a01f57ca183201
HarlanC <wawacry@qq.com>
2021-08-16 17:16:45 +0800

    refactor codes

--- protocol/rtmp/src/lib.rs
+++ protocol/rtmp/src/lib.rs
@@ -9,7 +9,7 @@ extern crate sha2;
 extern crate tokio;
 
 pub mod amf0;
-pub mod relay;
+pub mod cache;
 pub mod channels;
 pub mod chunk;
 pub mod config;
@@ -18,7 +18,8 @@ pub mod messages;
 pub mod netconnection;
 pub mod netstream;
 pub mod protocol_control_messages;
+pub mod relay;
+pub mod rtmp;
 pub mod session;
 pub mod user_control_messages;
 pub mod utils;
-pub mod cache;
