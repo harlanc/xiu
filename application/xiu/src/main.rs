817e19dd098e2b14bdcd5faaf37c29e6be634aaa
Harlan <wawacry@qq.com>
2023-02-08 22:56:50 +0800

    add CLI

--- application/xiu/src/main.rs
+++ application/xiu/src/main.rs
@@ -1,10 +1,7 @@
 use {
     //https://rustcc.cn/article?id=6dcbf032-0483-4980-8bfe-c64a7dfb33c7
     anyhow::Result,
-    clap::{value_parser, Arg, ArgGroup, Command},
-
-    env_logger::{Builder, Env, Target},
-
+    clap::{value_parser, Arg, Command},
     hls::server as hls_server,
     httpflv::server as httpflv_server,
     rtmp::{
@@ -13,13 +10,9 @@ use {
         rtmp::RtmpServer,
     },
     std::env,
-    std::path::Path,
     tokio,
     tokio::signal,
-    xiu::{
-        config::{config, config::Config},
-        logger::logger::{FileTarget, Rotate},
-    },
+    xiu::config::{config, config::Config},
 };
 
 //use application::logger::logger;
@@ -27,7 +20,7 @@ use hls::rtmp_event_processor::RtmpEventProcessor;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cmd = Command::new("XIU")
+    let mut cmd = Command::new("XIU")
         .bin_name("xiu")
         .version("0.1.0")
         .author("HarlanC <harlanc@foxmail.com>")
@@ -38,87 +31,36 @@ async fn main() -> Result<()> {
                 .short('c')
                 .value_name("path")
                 .help("Specify the xiu server configuration file path.")
-                .value_parser(value_parser!(String))
-               // .conflicts_with_all(["rtmp", "httpflv", "hls"]),
-        )
-        .arg(
-            Arg::new("rtmp")
-                .long("rtmp")
-                .short('p')
-                .value_name("port")
-                .help("Specify the RTMP listening port.")
-                .default_value("1935")
-                .value_parser(value_parser!(usize))
-                //.conflicts_with("config_file_path"),
-        )
-        .arg(
-            Arg::new("httpflv")
-                .long("httpflv")
-                .short('v')
-                .value_name("port")
-                .help("Specify the HTTP-FLV listening port.")
-                .value_parser(value_parser!(usize))
-                //.conflicts_with("config_file_path")
-                .default_value("8080"),
-        )
-        .arg(
-            Arg::new("hls")
-                .long("hls")
-                .short('s')
-                .value_name("port")
-                .help("Specify the HLS listening port.")
-                .value_parser(value_parser!(usize))
-                //.conflicts_with("config_file_path")
-                .default_value("8081"),
-        )
-        .group(
-            ArgGroup::new("vers")
-                .args(["config_file_path", "rtmp"])
-                .required(true),
+                .value_parser(value_parser!(String)),
         );
 
-
-
-    let matches = cmd.get_matches();
-
-    let config_file_path = &*matches.get_one("path").expect("msg");
-
-    // if let Some(config_file_path) = matches.get_one("path") {}
-
-    let config = config::load(config_file_path);
-    match config {
-        Ok(val) => {
-            /*set log level*/
-
-            // flexi_logger::Logger::try_with_env_or_str("info")?.start()?;
-            // if let Some(log_config_value) = &val.log {
-            //     flexi_logger::Logger::try_with_env_or_str(log_config_value.level.clone())?
-            //         .start()?;
-            // }
-            if let Some(log_config_value) = &val.log {
-                env::set_var("RUST_LOG", log_config_value.level.clone());
-            } else {
-                env::set_var("RUST_LOG", "info");
+    match cmd
+        .clone()
+        .get_matches()
+        .get_one::<String>("config_file_path")
+    {
+        Some(path) => {
+            let config = config::load(path);
+            match config {
+                Ok(val) => {
+                    /*set log level*/
+                    if let Some(log_config_value) = &val.log {
+                        env::set_var("RUST_LOG", log_config_value.level.clone());
+                    } else {
+                        env::set_var("RUST_LOG", "info");
+                    }
+                    env_logger::init();
+                    /*run the service*/
+                    let mut serivce = Service::new(val);
+                    serivce.run().await?;
+                }
+                _ => (),
             }
-
-            // let env = Env::default()
-            //     .filter_or("MY_LOG_LEVEL", "trace")
-            //     .write_style_or("MY_LOG_STYLE", "always");
-
-            // Builder::from_env(env)
-            //     .target(Target::Pipe(Box::new(FileTarget::new(
-            //         Rotate::Minute,
-            //         String::from("./logs"),
-            //     ))))
-            //     .init();
-
-            env_logger::init();
-
-            /*run the service*/
-            let mut serivce = Service::new(val);
-            serivce.run().await?;
         }
-        _ => (),
+        None => {
+            cmd.print_help()?;
+            return Ok(());
+        }
     }
 
     // log::info!("log info...");
