b38c63a8ec078f0e2efa6802ea7ee6a30c7e585c
Harlan <wawacry@qq.com>
2024-03-02 10:03:55 +0800

    some fix

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "====================================================="
-
+#sudo docker run -d -it --net=host --name xiu --privileged=true -v /Users/username/logs:/app/logs harlancn/xiu:0.1.49 /app/start.sh /app/config.toml 
 echo "Specify configuration file path"
-
 /app/xiu -c $1
